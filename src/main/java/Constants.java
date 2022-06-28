4ccf43b4bc0690a79c63a6fcb775f2e53c01eb0a
DanielChakmon <danielhakemon@gmail.com>
2022-06-28 21:06:41 +0300

    Initial commit

---
+++ src/main/java/Constants.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+public class Constants {
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int SOLID_COLOR_MAX_RGB_INDEX = 255;
+    public static final double TENTH = 0.1;
+    public static final int GREY_SCALE = 0;
+    public static final int MIRROR = 1;
+    public static final int COLOR_SHIFT_RIGHT = 2;
+    public static final int NEGATIVE = 3;
+    public static final int SEPIA = 4;
+    public static final int LIGHTER = 5;
+    public static final int WINDOW_WIDTH = 1200;
+    public static final int WINDOW_HEIGHT = 650;
+    public static JFrame myWindow;
+    public static final int LABEL_WIDTH = 110;
+    public static final int LABEL_HEIGHT = 30;
+    public static final int SPACE_BETWEEN_BOXES = 30;
+    public static final int SPACE_BETWEEN_LINES = 20;
+    public static final int TEXT_FIELD_WIDTH = 100;
+    public static final int TEXT_FIELD_HEIGHT = 30;
+    public static final int BUTTON_WIDTH = 100;
+    public static final int BUTTON_HEIGHT = 30;
+    public static final int MARGIN_FROM_TOP = 10;
+    public static final int MARGIN_FROM_LEFT = 5;
+    public static final int IMAGE_MAX_WIDTH = 500;
+    public static final int IMAGE_MAX_HEIGHT=500;
+    public static final int BIG_TEXT_SIZE=20;
+
+    public static void getWindowData(JFrame window) {
+        myWindow = window;
+    }
+}
---
+++ src/main/java/Filters.java
@@ -0,0 +1,121 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Filters {
+    private int filterIndex;
+    private BufferedImage image;
+
+
+    public Filters(int filterIndex, BufferedImage image) {
+        this.filterIndex = filterIndex;
+        this.image = image;
+        filterApplier();
+    }
+
+    public void setFilterIndex(int filterIndex) {
+        this.filterIndex = filterIndex;
+        filterApplier();
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    private void filterApplier() {
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++){
+                switch (filterIndex){
+                    case Constants.GREY_SCALE:{
+                        greyScale(i,j);
+                        break;
+                    }
+                    case Constants.MIRROR:{
+                        mirror(i,j);
+                        break;
+                    }
+                    case Constants.COLOR_SHIFT_RIGHT:{
+                        colorShiftRight(i,j);
+                        break;
+                    }
+                    case Constants.NEGATIVE:{
+                        negative(i,j);
+                        break;
+                    }
+                    case Constants.SEPIA:{
+                        sepia(i,j);
+                        break;
+                    }
+                    case Constants.LIGHTER:{
+                        lighter(i,j);
+                        break;
+                    }
+                    default:{
+                        System.out.println("Invalid filter");
+                        break;
+                    }
+                }
+            }
+        }
+        Constants.myWindow.repaint();
+    }
+
+    private void greyScale(int x, int y) {
+        Color pixelColor = new Color(image.getRGB(x, y));
+        int greyScaledColorRatio = (pixelColor.getBlue() + pixelColor.getRed() + pixelColor.getGreen()) / Constants.THREE;
+        Color greyScaledPixelColor = new Color(greyScaledColorRatio, greyScaledColorRatio, greyScaledColorRatio);
+        image.setRGB(x, y, greyScaledPixelColor.getRGB());
+    }
+
+    private void mirror(int x, int y) {
+        image.setRGB(image.getWidth() - x, y, image.getRGB(x, y));
+    }
+
+    private void colorShiftRight(int x, int y) {
+        Color pixelColor = new Color(image.getRGB(x, y));
+        int newRed = pixelColor.getBlue();
+        int newGreen = pixelColor.getRed();
+        int newBlue = pixelColor.getGreen();
+        Color rightShiftedColor = new Color(newRed, newGreen, newBlue);
+        image.setRGB(x, y, rightShiftedColor.getRGB());
+    }
+
+    private void negative(int x, int y) {
+        Color pixelColor = new Color(image.getRGB(x, y));
+        int newRed = Constants.SOLID_COLOR_MAX_RGB_INDEX - pixelColor.getRed();
+        int newGreen = Constants.SOLID_COLOR_MAX_RGB_INDEX - pixelColor.getGreen();
+        int newBlue = Constants.SOLID_COLOR_MAX_RGB_INDEX - pixelColor.getBlue();
+        Color negativeColor = new Color(newRed, newGreen, newBlue);
+        image.setRGB(x, y, negativeColor.getRGB());
+    }
+
+    private void sepia(int x, int y) {
+        Color pixelColor = new Color(image.getRGB(x, y));
+        int[] newRGB = new int[Constants.THREE];
+        final int newRed = Constants.ZERO;
+        final int newGreen = Constants.ONE;
+        final int newBlue = Constants.TWO;
+        newRGB[newRed] = (int) (0.393 * pixelColor.getRed() + 0.769 * pixelColor.getGreen() + 0.189 * pixelColor.getBlue());
+        newRGB[newGreen] = (int) (0.349 * pixelColor.getRed() + 0.686 * pixelColor.getGreen() + 0.168 * pixelColor.getBlue());
+        newRGB[newBlue] = (int) (0.272 * pixelColor.getRed() + 0.534 * pixelColor.getGreen() + 0.131 * pixelColor.getBlue());
+        for (int i = 0; i < Constants.THREE; i++) {
+            if (newRGB[i] > Constants.SOLID_COLOR_MAX_RGB_INDEX) {
+                newRGB[i] = Constants.SOLID_COLOR_MAX_RGB_INDEX;
+            }
+        }
+        Color sepiaColor = new Color(newRGB[newRed], newRGB[newGreen], newRGB[newBlue]);
+        image.setRGB(x, y, sepiaColor.getRGB());
+    }
+
+    private void lighter(int x, int y) {
+        Color pixelColor = new Color(image.getRGB(x, y));
+        int[] newRGB = new int[Constants.THREE];
+        final int newRed = Constants.ZERO;
+        final int newGreen = Constants.ONE;
+        final int newBlue = Constants.TWO;
+        newRGB[newRed] = (int) (pixelColor.getRed() + ((double) (Constants.SOLID_COLOR_MAX_RGB_INDEX - pixelColor.getRed())) * Constants.TENTH);
+        newRGB[newGreen] = (int) (pixelColor.getGreen() + ((double) (Constants.SOLID_COLOR_MAX_RGB_INDEX - pixelColor.getGreen())) * Constants.TENTH);
+        newRGB[newBlue] = (int) (pixelColor.getBlue() + ((double) (Constants.SOLID_COLOR_MAX_RGB_INDEX - pixelColor.getBlue())) * Constants.TENTH);
+        Color lighterColor = new Color(newRGB[newRed], newRGB[newGreen], newRGB[newBlue]);
+        image.setRGB(x, y, lighterColor.getRGB());
+    }
+}
